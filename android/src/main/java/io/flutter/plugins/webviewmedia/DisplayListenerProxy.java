41251a4e3f0e55015fec4367e4e2580aadff0258
Sait Banazılı <saitbnzl@gmail.com>
2020-03-08 02:48:06 +0300

    change package name

--- android/src/main/java/io/flutter/plugins/webviewmedia/DisplayListenerProxy.java
+++ android/src/main/java/io/flutter/plugins/webviewmedia/DisplayListenerProxy.java
@@ -1,4 +1,4 @@
-package io.flutter.plugins.webviewflutter;
+package io.flutter.plugins.webviewmedia;
 
 import static android.hardware.display.DisplayManager.DisplayListener;
 
--- android/src/main/java/io/flutter/plugins/webviewmedia/WebViewFactory.java
+++ android/src/main/java/io/flutter/plugins/webviewmedia/WebViewFactory.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package io.flutter.plugins.webviewflutter;
+package io.flutter.plugins.webviewmedia;
 
 import android.content.Context;
 import android.view.View;
