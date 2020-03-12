36354c2e77544a6584fa1a93d8e9685d6bce0a21
Sait Banazılı <saitbnzl@gmail.com>
2020-03-12 05:36:40 +0300

    rename WebViewFlutterPlugin -> WebViewMediaFlutterPlugin

--- example/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/EmbeddingV1Activity.java
+++ example/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/EmbeddingV1Activity.java
@@ -2,19 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package io.flutter.plugins.webviewflutterexample;
+package io.flutter.plugins.webviewmediaexample;
 
 import android.os.Bundle;
 import dev.flutter.plugins.e2e.E2EPlugin;
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.webviewflutter.WebViewFlutterPlugin;
+import io.flutter.plugins.webviewmedia.WebViewMediaFlutterPlugin;
 
 public class EmbeddingV1Activity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     E2EPlugin.registerWith(registrarFor("dev.flutter.plugins.e2e.E2EPlugin"));
-    WebViewFlutterPlugin.registerWith(
-        registrarFor("io.flutter.plugins.webviewflutter.WebViewFlutterPlugin"));
+    WebViewMediaFlutterPlugin.registerWith(
+        registrarFor("io.flutter.plugins.webviewmedia.WebViewMediaFlutterPlugin"));
   }
 }
